cc9854ce934ae81a9a6a796c6ae9b7c9d4a1847d
nopnop2002 <hau26280@tree.odn.ne.jp>
2024-09-26 11:29:16 +0900

    added tusb-serial project

---
+++ tusb-serial/README.md
@@ -0,0 +1,95 @@
+# tinyusb Example   
+This is LoRa and tinyusb gateway application.   
+```
+           +------------+          +------------+          +------------+
+           |            |          |            |          |            |
+           | USB Writer |--(USB)-->|    ESP32   |--(SPI)-->|   SX127x   |==(LoRa)==>
+           |            |          |            |          |            |
+           +------------+          +------------+          +------------+
+
+           +------------+          +------------+          +------------+
+           |            |          |            |          |            |
+==(LoRa)==>|   SX127x   |--(SPI)-->|    ESP32   |--(USB)-->| USB Reader |
+           |            |          |            |          |            |
+           +------------+          +------------+          +------------+
+```
+
+# Hardware requirements
+1. ESP32-S2/S3 Development board   
+Because the ESP32-S2/S3 does support USB OTG.   
+
+2. USB Connector   
+I used this:   
+![usb-connector](https://user-images.githubusercontent.com/6020549/124848149-3714ba00-dfd7-11eb-8344-8b120790c5c5.JPG)
+
+```
+ESP32-S2/S3 BOARD          USB CONNECTOR
+                           +--+
+                           | || VCC
+    [GPIO 19]    --------> | || D-
+    [GPIO 20]    --------> | || D+
+    [  GND  ]    --------> | || GND
+                           +--+
+```
+
+
+# Configuration
+![config-top](https://github.com/user-attachments/assets/55d44b46-630a-45d7-afc7-a6841ae32298)
+
+### USB to LoRa
+Subscribe with USB and send to LoRa.   
+ESP32 acts as USB Reader.   
+You can use this script as USB Writer.   
+```python3 ./write.py```
+
+```
+           +------------+          +------------+          +------------+
+           |            |          |            |          |            |
+           | USB Writer |--(USB)-->|    ESP32   |--(SPI)-->|   SX127x   |==(LoRa)==>
+           |            |          |            |          |            |
+           +------------+          +------------+          +------------+
+```
+
+![config-radio-1](https://github.com/user-attachments/assets/8506f4a6-8263-4832-9a13-98a27b55e5d2)
+
+Communicate with Arduino Environment.   
+Run this sketch.   
+ArduinoCode\Ra01S_RX   
+
+
+### LoRa to USB
+Receive from LoRa and publish as USB.   
+ESP32 acts as USB Writer.   
+You can use this script as USB Reader.   
+```python3 ./read.py```
+
+```
+           +------------+          +------------+          +------------+
+           |            |          |            |          |            |
+==(LoRa)==>|   SX127x   |--(SPI)-->|    ESP32   |--(USB)-->| USB Reader |
+           |            |          |            |          |            |
+           +------------+          +------------+          +------------+
+```
+
+![config-radio-2](https://github.com/user-attachments/assets/dd6440eb-c4f7-46e0-ad51-e05b6130f378)
+
+Communicate with Arduino Environment.   
+Run this sketch.   
+ArduinoCode\Ra01S_TX   
+
+
+# Using Windows Terminal Software
+When you connect the USB cable to the USB port on your Windows machine and build the firmware, a new COM port will appear.   
+Open a new COM port in the terminal software.   
+When set to USB to LoRa, input data from the keyboard is sent to LoRa.   
+I used TeraTerm.   
+![tusb-TeraTerm](https://github.com/user-attachments/assets/5abec7b7-bfe8-42c1-b945-106e9357035a)
+
+# Using Linux Terminal Software
+When you connect the USB cable to the USB port on your Linux machine and build the firmware, a new /dev/tty device will appear.   
+Open a new tty device in the terminal software.   
+Most occasions, the device is /dev/ttyACM0.   
+When set to USB to LoRa, input data from the keyboard is sent to LoRa.   
+I used screen.   
+![tusb-screen](https://github.com/user-attachments/assets/dd6fb0bb-a0af-4ebe-9b1b-bb2991cf1b5e)
+
---
+++ tusb-serial/main/main.c
@@ -0,0 +1,241 @@
+/* The example of ESP-IDF
+ *
+ * This sample code is in the public domain.
+ */
+
+#include <stdio.h>
+#include <inttypes.h>
+#include <string.h>
+
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "freertos/message_buffer.h"
+#include "tinyusb.h"
+#include "tusb_cdc_acm.h"
+#include "esp_log.h"
+
+#include "lora.h"
+
+static const char *TAG = "MAIN";
+
+MessageBufferHandle_t xMessageBufferTrans;
+MessageBufferHandle_t xMessageBufferRecv;
+QueueHandle_t xQueueTinyusb;
+
+// The total number of bytes (not single messages) the message buffer will be able to hold at any one time.
+size_t xBufferSizeBytes = 1024;
+
+void tinyusb_cdc_rx_callback(int itf, cdcacm_event_t *event)
+{
+	/* initialization */
+	size_t rx_size = 0;
+	ESP_LOGD(TAG, "CONFIG_TINYUSB_CDC_RX_BUFSIZE=%d", CONFIG_TINYUSB_CDC_RX_BUFSIZE);
+
+	/* read */
+	uint8_t buf[CONFIG_TINYUSB_CDC_RX_BUFSIZE];
+	esp_err_t ret = tinyusb_cdcacm_read(itf, buf, CONFIG_TINYUSB_CDC_RX_BUFSIZE, &rx_size);
+	if (ret == ESP_OK) {
+		ESP_LOGD(TAG, "Data from channel=%d rx_size=%d", itf, rx_size);
+		ESP_LOG_BUFFER_HEXDUMP(TAG, buf, rx_size, ESP_LOG_INFO);
+		for(int i=0;i<rx_size;i++) {
+			xQueueSendFromISR(xQueueTinyusb, &buf[i], NULL);
+		}
+	} else {
+		ESP_LOGE(TAG, "tinyusb_cdcacm_read error");
+	}
+}
+
+void tinyusb_cdc_line_state_changed_callback(int itf, cdcacm_event_t *event)
+{
+	int dtr = event->line_state_changed_data.dtr;
+	int rts = event->line_state_changed_data.rts;
+	ESP_LOGI(TAG, "Line state changed on channel %d: DTR:%d, RTS:%d", itf, dtr, rts);
+}
+
+
+#if CONFIG_SENDER
+void task_tx(void *pvParameters)
+{
+	ESP_LOGI(pcTaskGetName(NULL), "Start");
+	uint8_t buf[256]; // Maximum Payload size of SX1276/77/78/79 is 255
+	while(1) {
+		size_t received = xMessageBufferReceive(xMessageBufferRecv, buf, sizeof(buf), portMAX_DELAY);
+		ESP_LOGI(pcTaskGetName(NULL), "xMessageBufferReceive received=%d", received);
+
+		lora_send_packet(buf, received);
+		ESP_LOGI(pcTaskGetName(NULL), "%d byte packet sent...", received);
+		int lost = lora_packet_lost();
+		if (lost != 0) {
+			ESP_LOGW(pcTaskGetName(NULL), "%d packets lost", lost);
+		}
+	} // end while
+	vTaskDelete(NULL);
+}
+
+void usb_rx(void *pvParameters)
+{
+	ESP_LOGI(pcTaskGetName(NULL), "Start");
+	char buffer[256]; // Maximum Payload size of SX1261/62/68 is 255
+	int index = 0;
+	while(1) {
+		char ch;
+		if(xQueueReceive(xQueueTinyusb, &ch, portMAX_DELAY)) {
+			ESP_LOGD(pcTaskGetName(NULL), "ch=0x%x",ch);
+			if (ch == 0x0d || ch == 0x0a) {
+				if (index > 0) {
+					ESP_LOGI(pcTaskGetName(NULL), "[%.*s]", index, buffer);
+					size_t spacesAvailable = xMessageBufferSpacesAvailable( xMessageBufferRecv );
+					ESP_LOGI(pcTaskGetName(NULL), "spacesAvailable=%d", spacesAvailable);
+					size_t sended = xMessageBufferSend(xMessageBufferRecv, &buffer, index, 100);
+					if (sended != index) {
+						ESP_LOGE(pcTaskGetName(NULL), "xMessageBufferSend fail index=%d sended=%d", index, sended);
+						break;
+					}
+					index = 0;
+				}
+			} else {
+				if (index == 255) continue;
+				buffer[index++] = ch;
+			}
+		}
+	} // end while
+	vTaskDelete(NULL);
+}
+#endif // CONFIG_SENDER
+
+#if CONFIG_RECEIVER
+void task_rx(void *pvParameters)
+{
+	ESP_LOGI(pcTaskGetName(NULL), "Start");
+	uint8_t buf[256]; // Maximum Payload size of SX1276/77/78/79 is 255
+	while(1) {
+		lora_receive(); // put into receive mode
+		if (lora_received()) {
+			int rxLen = lora_receive_packet(buf, sizeof(buf));
+			ESP_LOGI(pcTaskGetName(NULL), "%d byte packet received:[%.*s]", rxLen, rxLen, buf);
+
+			size_t spacesAvailable = xMessageBufferSpacesAvailable( xMessageBufferTrans );
+			ESP_LOGI(pcTaskGetName(NULL), "spacesAvailable=%d", spacesAvailable);
+			size_t sended = xMessageBufferSend(xMessageBufferTrans, buf, rxLen, 100);
+			if (sended != rxLen) {
+				ESP_LOGE(pcTaskGetName(NULL), "xMessageBufferSend fail rxLen=%d sended=%d", rxLen, sended);
+				break;
+			}
+		}
+		vTaskDelay(1); // Avoid WatchDog alerts
+	} // end while
+	vTaskDelete(NULL);
+}
+
+void usb_tx(void *pvParameters)
+{
+	ESP_LOGI(pcTaskGetName(NULL), "Start");
+	uint8_t buf[256]; // Maximum Payload size of SX1261/62/68 is 255
+	uint8_t crlf[2] = { 0x0d, 0x0a };
+	while(1) {
+		size_t received = xMessageBufferReceive(xMessageBufferTrans, buf, sizeof(buf), portMAX_DELAY);
+		ESP_LOGI(pcTaskGetName(NULL), "%d byte packet received:[%.*s]", received, received, buf);
+		tinyusb_cdcacm_write_queue(TINYUSB_CDC_ACM_0, buf, received);
+		tinyusb_cdcacm_write_queue(TINYUSB_CDC_ACM_0, crlf, 2);
+		tinyusb_cdcacm_write_flush(TINYUSB_CDC_ACM_0, 0);
+	} // end while
+	vTaskDelete(NULL);
+}
+#endif // CONFIG_RECEIVER
+
+void app_main()
+{
+	ESP_LOGI(TAG, "USB initialization");
+	const tinyusb_config_t tusb_cfg = {
+		.device_descriptor = NULL,
+		.string_descriptor = NULL,
+		.external_phy = false,
+		.configuration_descriptor = NULL,
+	};
+	ESP_ERROR_CHECK(tinyusb_driver_install(&tusb_cfg));
+
+	tinyusb_config_cdcacm_t acm_cfg = {
+		.usb_dev = TINYUSB_USBDEV_0,
+		.cdc_port = TINYUSB_CDC_ACM_0,
+		.rx_unread_buf_sz = 64,
+		.callback_rx = &tinyusb_cdc_rx_callback, // the first way to register a callback
+		.callback_rx_wanted_char = NULL,
+		.callback_line_state_changed = &tinyusb_cdc_line_state_changed_callback,
+		.callback_line_coding_changed = NULL
+	};
+	ESP_ERROR_CHECK(tusb_cdc_acm_init(&acm_cfg));
+
+	// Create MessageBuffer
+	xMessageBufferTrans = xMessageBufferCreate(xBufferSizeBytes);
+	configASSERT( xMessageBufferTrans );
+	xMessageBufferRecv = xMessageBufferCreate(xBufferSizeBytes);
+	configASSERT( xMessageBufferRecv );
+
+	// Create Queue
+	xQueueTinyusb = xQueueCreate(100, sizeof(char));
+	configASSERT( xQueueTinyusb );
+
+	// Initialize LoRa
+	if (lora_init() == 0) {
+		ESP_LOGE(pcTaskGetName(NULL), "Does not recognize the module");
+		while(1) {
+			vTaskDelay(1);
+		}
+	}
+
+#if CONFIG_169MHZ
+	ESP_LOGI(pcTaskGetName(NULL), "Frequency is 169MHz");
+	lora_set_frequency(169e6); // 169MHz
+#elif CONFIG_433MHZ
+	ESP_LOGI(pcTaskGetName(NULL), "Frequency is 433MHz");
+	lora_set_frequency(433e6); // 433MHz
+#elif CONFIG_470MHZ
+	ESP_LOGI(pcTaskGetName(NULL), "Frequency is 470MHz");
+	lora_set_frequency(470e6); // 470MHz
+#elif CONFIG_866MHZ
+	ESP_LOGI(pcTaskGetName(NULL), "Frequency is 866MHz");
+	lora_set_frequency(866e6); // 866MHz
+#elif CONFIG_915MHZ
+	ESP_LOGI(pcTaskGetName(NULL), "Frequency is 915MHz");
+	lora_set_frequency(915e6); // 915MHz
+#elif CONFIG_OTHER
+	ESP_LOGI(pcTaskGetName(NULL), "Frequency is %dMHz", CONFIG_OTHER_FREQUENCY);
+	long frequency = CONFIG_OTHER_FREQUENCY * 1000000;
+	lora_set_frequency(frequency);
+#endif
+
+	lora_enable_crc();
+
+	int cr = 1;
+	int bw = 7;
+	int sf = 7;
+#if CONFIF_ADVANCED
+	cr = CONFIG_CODING_RATE
+	bw = CONFIG_BANDWIDTH;
+	sf = CONFIG_SF_RATE;
+#endif
+
+	lora_set_coding_rate(cr);
+	//lora_set_coding_rate(CONFIG_CODING_RATE);
+	//cr = lora_get_coding_rate();
+	ESP_LOGI(pcTaskGetName(NULL), "coding_rate=%d", cr);
+
+	lora_set_bandwidth(bw);
+	//lora_set_bandwidth(CONFIG_BANDWIDTH);
+	//int bw = lora_get_bandwidth();
+	ESP_LOGI(pcTaskGetName(NULL), "bandwidth=%d", bw);
+
+	lora_set_spreading_factor(sf);
+	//lora_set_spreading_factor(CONFIG_SF_RATE);
+	//int sf = lora_get_spreading_factor();
+	ESP_LOGI(pcTaskGetName(NULL), "spreading_factor=%d", sf);
+
+#if CONFIG_SENDER
+	xTaskCreate(&task_tx, "TX", 1024*3, NULL, 5, NULL);
+	xTaskCreate(&usb_rx, "USB_RX", 1024*4, NULL, 5, NULL);
+#endif
+#if CONFIG_RECEIVER
+	xTaskCreate(&task_rx, "RX", 1024*3, NULL, 5, NULL);
+	xTaskCreate(&usb_tx, "USB_TX", 1024*4, NULL, 5, NULL);
+#endif
+}
