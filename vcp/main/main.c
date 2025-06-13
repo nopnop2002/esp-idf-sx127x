41791485c574b9dc30eef24319c2a17447e0672a
nopnop2002 <>
2025-06-14 08:00:49 +0900

    added vcp project

---
+++ vcp/main/main.c
@@ -0,0 +1,152 @@
+/* The example of ESP-IDF
+ *
+ * This sample code is in the public domain.
+ */
+
+#include <stdio.h>
+#include <inttypes.h>
+#include <string.h>
+
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "freertos/message_buffer.h"
+#include "esp_log.h"
+
+#include "lora.h"
+
+static const char *TAG = "MAIN";
+
+MessageBufferHandle_t xMessageBufferTx;
+MessageBufferHandle_t xMessageBufferRx;
+
+// The total number of bytes (not messages) the message buffer will be able to hold at any one time.
+size_t xBufferSizeBytes = 1024;
+// The size, in bytes, required to hold each item in the message,
+size_t xItemSize = 256; // Maximum Payload size of SX1261/62/68 is 255
+
+#if CONFIG_SENDER
+void task_tx(void *pvParameters)
+{
+	ESP_LOGI(pcTaskGetName(NULL), "Start");
+	uint8_t buf[256]; // Maximum Payload size of SX1276/77/78/79 is 255
+	while(1) {
+		size_t received = xMessageBufferReceive(xMessageBufferRx, buf, sizeof(buf), portMAX_DELAY);
+		ESP_LOGI(pcTaskGetName(NULL), "xMessageBufferReceive received=%d", received);
+		ESP_LOG_BUFFER_HEXDUMP(pcTaskGetName(NULL), buf, received, ESP_LOG_DEBUG);
+
+		// The VCP termination character is CR+LF. So Remove CR/LF.
+		int sending = 0;
+		for (int i=0;i<received;i++) {
+			if (buf[i] == 0x0d) continue;
+			if (buf[i] == 0x0a) continue;
+			sending++;
+		}
+		ESP_LOG_BUFFER_HEXDUMP(pcTaskGetName(NULL), buf, sending, ESP_LOG_INFO);
+
+		lora_send_packet(buf, sending);
+		ESP_LOGI(pcTaskGetName(NULL), "%d byte packet sent...", sending);
+		int lost = lora_packet_lost();
+		if (lost != 0) {
+			ESP_LOGW(pcTaskGetName(NULL), "%d packets lost", lost);
+		}
+	} // end while
+}
+#endif // CONFIG_SENDER
+
+#if CONFIG_RECEIVER
+void task_rx(void *pvParameters)
+{
+	ESP_LOGI(pcTaskGetName(NULL), "Start");
+	uint8_t buf[256]; // Maximum Payload size of SX1276/77/78/79 is 255
+	while(1) {
+		lora_receive(); // put into receive mode
+		if (lora_received()) {
+			int rxLen = lora_receive_packet(buf, sizeof(buf));
+			ESP_LOGI(pcTaskGetName(NULL), "%d byte packet received:[%.*s]", rxLen, rxLen, buf);
+
+			int txLen = rxLen;
+			if (buf[txLen-1] != 0x0a) {
+				buf[txLen] = 0x0a;
+				txLen++;
+			}
+			size_t spacesAvailable = xMessageBufferSpacesAvailable( xMessageBufferTx );
+			ESP_LOGI(pcTaskGetName(NULL), "spacesAvailable=%d", spacesAvailable);
+			size_t sended = xMessageBufferSend(xMessageBufferTx, buf, txLen, 100);
+			if (sended != txLen) {
+				ESP_LOGE(pcTaskGetName(NULL), "xMessageBufferSend fail rxLen=%d sended=%d", rxLen, sended);
+				break;
+			}
+		}
+		vTaskDelay(1); // Avoid WatchDog alerts
+	} // end while
+}
+#endif // CONFIG_RECEIVER
+
+void cdc_acm_vcp_task(void *pvParameters);
+
+void app_main()
+{
+	// Create MessageBuffer
+	xMessageBufferTx = xMessageBufferCreate(xBufferSizeBytes);
+	configASSERT( xMessageBufferTx );
+	xMessageBufferRx = xMessageBufferCreate(xBufferSizeBytes);
+	configASSERT( xMessageBufferRx );
+
+	// Initialize LoRa
+	if (lora_init() == 0) {
+		ESP_LOGE(pcTaskGetName(NULL), "Does not recognize the module");
+		while(1) {
+			vTaskDelay(1);
+		}
+	}
+
+#if CONFIG_433MHZ
+	ESP_LOGI(pcTaskGetName(NULL), "Frequency is 433MHz");
+	lora_set_frequency(433e6); // 433MHz
+#elif CONFIG_866MHZ
+	ESP_LOGI(pcTaskGetName(NULL), "Frequency is 866MHz");
+	lora_set_frequency(866e6); // 866MHz
+#elif CONFIG_915MHZ
+	ESP_LOGI(pcTaskGetName(NULL), "Frequency is 915MHz");
+	lora_set_frequency(915e6); // 915MHz
+#elif CONFIG_OTHER
+	ESP_LOGI(pcTaskGetName(NULL), "Frequency is %dMHz", CONFIG_OTHER_FREQUENCY);
+	long frequency = CONFIG_OTHER_FREQUENCY * 1000000;
+	lora_set_frequency(frequency);
+#endif
+
+	lora_enable_crc();
+
+	int cr = 1;
+	int bw = 7;
+	int sf = 7;
+#if CONFIG_ADVANCED
+	cr = CONFIG_CODING_RATE;
+	bw = CONFIG_BANDWIDTH;
+	sf = CONFIG_SF_RATE;
+#endif
+
+	lora_set_coding_rate(cr);
+	//lora_set_coding_rate(CONFIG_CODING_RATE);
+	//cr = lora_get_coding_rate();
+	ESP_LOGI(pcTaskGetName(NULL), "coding_rate=%d", cr);
+
+	lora_set_bandwidth(bw);
+	//lora_set_bandwidth(CONFIG_BANDWIDTH);
+	//int bw = lora_get_bandwidth();
+	ESP_LOGI(pcTaskGetName(NULL), "bandwidth=%d", bw);
+
+	lora_set_spreading_factor(sf);
+	//lora_set_spreading_factor(CONFIG_SF_RATE);
+	//int sf = lora_get_spreading_factor();
+	ESP_LOGI(pcTaskGetName(NULL), "spreading_factor=%d", sf);
+
+#if CONFIG_SENDER
+	xTaskCreate(&task_tx, "TX", 1024*3, NULL, 5, NULL);
+#endif
+#if CONFIG_RECEIVER
+	xTaskCreate(&task_rx, "RX", 1024*3, NULL, 5, NULL);
+#endif
+	// Start CDC_ACM_VCP
+	xTaskCreate(cdc_acm_vcp_task, "CDC_ACM_VCP", 1024*4, NULL, 5, NULL);
+}
