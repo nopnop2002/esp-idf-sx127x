ca682da3a8c4fab9fe7b38b555b04b40a17e22de
nopnop2002 <>
2025-06-18 17:01:02 +0900

    added xItemSize

--- http/main/http_server.c
+++ http/main/http_server.c
@@ -19,6 +19,7 @@
 static const char *TAG = "SERVER";
 
 extern MessageBufferHandle_t xMessageBufferRecv;
+extern size_t xItemSize;
 
 /* root post handler */
 static esp_err_t root_post_handler(httpd_req_t *req)
@@ -45,7 +46,8 @@ static esp_err_t root_post_handler(httpd_req_t *req)
 
 	// Queries a message buffer to see how much free space it contains
 	size_t spacesAvailable = xMessageBufferSpacesAvailable( xMessageBufferRecv );
-	ESP_LOGI(pcTaskGetName(NULL), "spacesAvailable=%d", spacesAvailable);
+	ESP_LOGI(TAG, "spacesAvailable=%d", spacesAvailable);
+	if (req->content_len > xItemSize) req->content_len = xItemSize;
 	size_t sended = xMessageBufferSend(xMessageBufferRecv, buf, req->content_len, 100);
 	if (sended != req->content_len) {
 		ESP_LOGE(TAG, "xMessageBufferSend fail. sended=%d req->content_len=%d", sended, req->content_len);
@@ -103,6 +105,8 @@ esp_err_t start_server(int port)
 
 	return ESP_OK;
 }
+
+
 
 void http_server(void *pvParameters)
 {
